e2bc2def73bd279feec21c4ba52dc58ea35a3ead
Tianyu Geng <tgeng@google.com>
2018-01-01 14:44:19 -0800

    update

--- bin/lock.sh
+++ bin/lock.sh
@@ -2,7 +2,11 @@
 revert() {
   xset dpms 0 0 0
 }
-trap revert SIGHUP SIGINT SIGTERM
-xset +dpms dpms 5 5 5
-i3lock -n
-revert
+if command -v xfce4-session-logout; then
+  xfce4-session-logout --suspend
+else
+  trap revert SIGHUP SIGINT SIGTERM
+  xset +dpms dpms 5 5 5
+  i3lock -n
+  revert
+fi
