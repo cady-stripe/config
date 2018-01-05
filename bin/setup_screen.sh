8ace87a83d22aada1eea5c3413fb131c68ddff62
Tianyu Geng <tgeng@google.com>
2018-01-04 20:27:41 -0800

    update

--- bin/setup_screen.sh
+++ bin/setup_screen.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
-until [ xrandr ];
-do sleep 1
-done
+#!/bin/bash
+set -x
+
+export XAUTHORITY=/home/tgeng/.Xauthority
+export DISPLAY=':0.0'
 
 while true
 do
@@ -9,7 +10,7 @@ monitors=$(xrandr --query | grep " connected")
 if echo $monitors | grep 'DVI-D-1 connected' && echo $monitors | grep 'DP-1 connected'; then
   echo 'both monitors are connected'
   xrandr --output DP-1 --primary --mode 3440x1440 --pos 0x0 --rotate normal --output DVI-D-1 --mode 1920x1200 --pos 3440x0 --rotate left
-  feh --bg-fill ~/Pictures/wallpaper.jpg
+  feh --bg-fill /home/tgeng/Pictures/wallpaper.jpg
   exit
 fi
 sleep 1
