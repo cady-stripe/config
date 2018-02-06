34800adf13ec877938b0cd2363a23bc39abc45bc
Tianyu Geng <tgeng@google.com>
2018-02-05 21:36:33 -0800

    update

--- dotfiles/.zsh_tgeng_extra/git.zsh
+++ dotfiles/.zsh_tgeng_extra/git.zsh
@@ -1,11 +1,3 @@
 alias gb='git for-each-ref --sort=committerdate refs/heads/ --format='\''%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'\'''
-alias gc='git commit -v'
-alias gco='git checkout'
-alias gporm='git push origin HEAD:refs/for/master'
-alias gpr='git pull --rebase'
 alias gs='git status'
 alias gsu='git submodule update --init --recursive'
-_gcb() {
-  [ -n "$1"  ] && git checkout -b $1 && git branch --set-upstream-to origin/master || echo "You must provide a unique branch name."
-}
-alias gcb=_gcb
--- dotfiles/.zsh_tgeng_extra/google.zsh
+++ dotfiles/.zsh_tgeng_extra/google.zsh
@@ -1,29 +1,33 @@
-function sj() {
+if [[ -e /usr/local/google/home ]]; then
+  function sj() {
     if [[ $PWD =~ '(.*)/javatests(.*)' ]]; then
-        cd "${match[1]}/java${match[2]}"
+      cd "${match[1]}/java${match[2]}"
     else
-        cd "${PWD/\/google3\/java//google3/javatests}"
+      cd "${PWD/\/google3\/java//google3/javatests}"
     fi
-}
+  }
 
-function bb() {
-  if [[ $PWD =~ '(.*)/blaze-bin(.*)' ]]; then
-    cd "${match[1]}${match[2]}"
-  else
-    cd "${PWD/\/google3//google3/blaze-bin}"
-  fi
-}
+  function bb() {
+    if [[ $PWD =~ '(.*)/blaze-bin(.*)' ]]; then
+      cd "${match[1]}${match[2]}"
+    else
+      cd "${PWD/\/google3//google3/blaze-bin}"
+    fi
+  }
 
-alias cdg='bd google3'
-alias gme='g fix5 && g multi export'
-alias gl='g log5'
-alias gr='g rb5'
-alias gms='g multi sync'
-alias gcm='g commit --amend --no-edit'
-alias gcam='g commit -a --amend --no-edit'
-alias gca='g commit -a'
-alias gfix='g fix5'
-alias grc='g rebase --continue'
-alias gra='g rebase --abort'
-alias ga='noglob g add'
+  alias cdg='bd google3'
+  alias gme='g fix5 && g multi export'
+  alias gl='g log5'
+  alias gr='g rb5'
+  alias gms='g multi sync'
+  alias gcm='g commit --amend --no-edit'
+  alias gcam='g commit -a --amend --no-edit'
+  alias gca='g commit -a'
+  alias gfix='g fix5'
+  alias grc='g rebase --continue'
+  alias gra='g rebase --abort'
+  alias ga='noglob g add'
+  alias fixjs=/google/src/components/head/google3/third_party/java_src/jscomp/java/com/google/javascript/jscomp/lint/fixjs.sh
 
+  source /etc/bash_completion.d/g4d
+fi
