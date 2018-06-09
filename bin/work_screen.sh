bc2d0ef27bc702f0df066aecc81cfc8996fb560a
Tianyu Geng <tgeng@google.com>
2018-06-08 23:01:56 -0700

    work

---
+++ bin/work_screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-7 --off --output DP-6 --off --output DP-5 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-4 --mode 2560x1440 --pos 2560x0 --rotate right --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
