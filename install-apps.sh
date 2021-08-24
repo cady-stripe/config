bb12ddea0d42c4cf451fbc16232ce94e5012928e
tgeng <tgeng@google.com>
2021-08-24 09:55:05 -0700

    install font-awesome

--- install-apps.sh
+++ install-apps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt install i3-wm xdotool tmux cmake compton autokey-gtk feh rofi tilix terminator python3-pip fzf
+sudo apt install i3-wm xdotool tmux cmake compton autokey-gtk feh rofi tilix terminator python3-pip fzf fonts-font-awesome
 pip3 install --user i3ipc
